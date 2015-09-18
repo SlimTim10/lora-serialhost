e13bbb1a602940d71056f1d4c8a97683caac21e7
Tim Johns <tim.johns@icewire.ca>
2015-09-18 19:31:06 -0400

    Fix jumbled serial messages bug

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 var loraserialhostApp = angular.module("loraserialhostApp", []);
 
-loraserialhostApp.controller("MainCtrl", function ($scope) {
+loraserialhostApp.controller("MainCtrl", function($scope) {
 	$scope.title = "LoRa Serial Host";
 	$scope.log = "";
 	$scope.disconnectStyle = {
 		"display": "none"
 	};
+	var decoder = new TextDecoder();
 
 	$scope.closeApp = function() {
 		window.close();
 	};
 
 	$scope.refreshPorts = function() {
-		chrome.serial.getDevices(function (devices) {
-			$scope.$apply(function () {
+		chrome.serial.getDevices(function(devices) {
+			$scope.$apply(function() {
 				$scope.ports = [];
 				for (var i = 0; i < devices.length; i++) {
 					$scope.ports.push(devices[i].path);
@@ -23,33 +24,35 @@ loraserialhostApp.controller("MainCtrl", function ($scope) {
 	};
 	$scope.refreshPorts();
 
+	var readPort = function(info) {
+		$scope.$apply(function() {
+			$scope.log += decoder.decode(info.data);
+		});
+	};
+
 	$scope.connect = function(port) {
+		chrome.serial.onReceive.removeListener(readPort);
 		$scope.usePort = port;
 		chrome.serial.connect(port, {
 			bitrate: 9600,
 			receiveTimeout: 5000,
 			sendTimeout: 1000
 		}, function(info) {
-			$scope.$apply(function () {
+			$scope.$apply(function() {
 				$scope.connectionId = info.connectionId;
 			});
 		});
-		$scope.log = "Connected to " + $scope.usePort + "\n";
+		$scope.log = "Connected to " + $scope.usePort + "\n\n";
 		$scope.disconnectStyle = {
 			"display": "block"
 		};
 
-		var decoder = new TextDecoder();
-		chrome.serial.onReceive.addListener(function (info) {
-			$scope.$apply(function() {
-				$scope.log += decoder.decode(info.data);
-			});
-		});
+		chrome.serial.onReceive.addListener(readPort);
 	};
 
 	$scope.disconnect = function() {
 		chrome.serial.disconnect($scope.connectionId, function(result) {
-			$scope.$apply(function () {
+			$scope.$apply(function() {
 				if (result === true) {
 					$scope.log += "Disconnected";
 					$scope.disconnectStyle = {
